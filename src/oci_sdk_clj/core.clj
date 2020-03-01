2519bef778389d7a66ed628d64affc5ea313d80c
Owain Lewis <owain.lewis@oracle.com>
2020-03-01 17:11:55 +0000

    Signing stuff mostly working now

--- src/oci_sdk_clj/core.clj
+++ src/oci_sdk_clj/core.clj
@@ -1,7 +1,24 @@
 (ns oci-sdk-clj.core
-  (:import [com.oracle.bmc.auth ConfigFileAuthenticationDetailsProvider]
-           [com.oracle.bmc.http.signing DefaultRequestSigner]))
+  (:require [oci-sdk-clj.signing :as signing]
+            [java-http-clj.core :as http]))
 
-(defn authentication-provider
- ([] (authentication-provider "DEFAULT"))
- ([profile] (new ConfigFileAuthenticationDetailsProvider profile)))
+(defn sample-request [compartment]
+  {:body nil
+   :headers {}
+   :method :get
+   :uri (str "https://iaas.us-ashburn-1.oraclecloud.com"
+             "/20160918/instances?compartmentId="
+             compartment)})
+
+(def auth-provider (signing/config-file-authentication-provider))
+
+;; (defn sign-request-params [auth-provider uri-string method headers body]
+
+(defn go [auth-provider request]
+  (let [{uri :uri method :method headers :headers body :body} request
+        signed-headers (signing/sign-request-params auth-provider uri (name method) headers body)]
+    (assoc request :headers signed-headers)))
+
+(defn req [compartment]
+  (let [request (go auth-provider (sample-request compartment))]
+    request))
