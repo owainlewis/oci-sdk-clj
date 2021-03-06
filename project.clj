764c8733bc214e64890ffe402143c487eb5f2a91
Owain Lewis <owain.lewis@oracle.com>
2021-03-06 20:29:32 +0000

    Fix project ns

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject oci-sdk-clj "0.1.0-SNAPSHOT"
-  :description "Clojure SDK for Oracle Cloud Infrastructure"
+(defproject com.owainlewis/oci-sdk-clj "0.1.0-SNAPSHOT"
+  :description "Clojure SDK for Oracle Cloud Infrastructure (OCI)"
   :url "https://github.com/owainlewis/oci-sdk-clj"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
